f5914700da83c9950be13f8770a323cf78c7d09a
yizhichengit <yizhi.chen1@my.jcu.edu.au>
2023-10-04 15:12:45 +0800

    prac_04 homework

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,21 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+#numbers[0] = 3
+#numbers[-1] = 2
+#number[3] = 1
+'numbers[:-1] = [3, 1, 4 ,1 ,5 ,9]'
+'numbers[3:4] = [1]'
+'5 in numbers = True'
+'7 in numbers = False'
+'"3" in numbers = False'
+'numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]'
+numbers[0] = "ten"
+numbers.append(1)
+print(numbers[2:])
+number = 9
+if number in numbers:
+    print(f"{number} is an element of the numbers list")
+else:
+    print(f"{number} is not an element of the numbers list")
+
+
+
---
+++ prac_04/quick_picks.py
@@ -0,0 +1,26 @@
+import random
+
+NUM_QUICK_PICKS = 0
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+NUMBERS_PER_PICK = 6
+
+def generate_quick_pick():
+    # Generate a single quick pick without using random.sample()
+    quick_pick = []
+    while len(quick_pick) < NUMBERS_PER_PICK:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in quick_pick:
+            quick_pick.append(number)
+    quick_pick.sort()
+    return quick_pick
+
+def main():
+    num_quick_picks = int(input("How many quick picks? "))
+    for _ in range(num_quick_picks):
+        quick_pick = generate_quick_pick()
+        print(" ".join(map(str, quick_pick)))
+
+if __name__ == "__main__":
+    main()
+
